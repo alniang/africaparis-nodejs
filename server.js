bfec981daf09009a110709b573cd542a4a1fcfa0
Alpha Niang <alpha.niang@hotmail.com>
2020-07-12 11:23:42 +0200

    first commit

--- server.js
+++ server.js
@@ -20,17 +20,18 @@ app.use(jsonParser);
 app.use('/html', express.static(__dirname+"/html"));
 
 app.get('/', function(req , res ) {
-	res.redirect('/html/index.html');
+	// res.redirect('/html/index.html');
+	res.send("Bienvenue ")
 });
 
 app.use(eventsApiRoutes.apiRouter); //delegate REST API routes to apiRouter(s)
 
-/*app.listen(8282 , function () {
+app.listen(8282 , function () {
 	console.log("http://localhost:8282");
-});*/
+});
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
+// app.listen(port);
